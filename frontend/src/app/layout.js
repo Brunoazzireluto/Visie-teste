80ff98df20812a0ace7612507a25c41f1be57002
Bruno Alves <bruno.a@outlook.com.br>
2023-06-04 23:20:01 -0300

    starting front end project with Next.js

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({ weight: '400', subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Visie teste',
+  description: 'Um teste para fazer um frontend para a api criada',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
